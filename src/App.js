a0ca5f321de3984f0273f5e94885fde04c31704b
Jamshed Ahmed <jamsheda4ahmed786@gmail.com>
2020-06-06 23:42:56 +0500

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Jamshed Ahmed First React App <br></br>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="heading">Hello World!!</h1>
+      <p className="text">My name is <strong>Jamshed Ahmed</strong> and 
+      I am {15+4}</p>
     </div>
   );
 }
